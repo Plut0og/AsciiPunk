0dc01b816ce6fbcdbc4793c8b4ec8f5a07c70ea4
synctax <benwyngaard@gmail.com>
2018-11-22 14:49:08 -0500

    added first animation file and anim parser

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <chrono>
 #include <math.h>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
 
 void initcurses(){
 
@@ -21,13 +26,34 @@ void cleanup(){
 	return;
 }
 
+vector<string> getAnim(string fileName){
+    ifstream assetFile;
+    assetFile.open(fileName);
+
+    int frameHeight, frames;
+    assetFile >> frameHeight >> frames;
+
+    vector<string> anim;
+    string line, frame;
+    getline(assetFile, line);
+    for (int i = 0; i < frames; i++){
+        frame = "";
+        for (int j = 0; j < frameHeight; j++){
+            getline(assetFile,line);
+            frame += line + "\n";
+        }
+        anim.push_back(frame);
+    }
+    return anim;
+}
+
 int main(){
-	
+
 	initcurses();
 
 	int ch;
-	nodelay(stdscr, TRUE); 
-	
+	nodelay(stdscr, TRUE);
+
 	//init time features
 	std::chrono::high_resolution_clock::time_point start, c_time; //two time points start, and changing c-time
 
@@ -39,14 +65,14 @@ int main(){
 
 	start = std::chrono::high_resolution_clock::now();
 
-	//mainloop 
+	//mainloop
 	bool isRunning = true;
 	while(isRunning){
 
 		//handling game frames
 		c_time = std::chrono::high_resolution_clock::now();
 		delta_start = std::chrono::duration_cast< std::chrono::duration<double> >(c_time - start);
-		if(delta_start.count() > (delta_frame * c_frame)){			
+		if(delta_start.count() > (delta_frame * c_frame)){
 			//alternating "X" and "O" every second(30 frames)
 			if(((int)floor(c_frame / 30) % 2) == 0){
 				addstr("O");
@@ -65,7 +91,7 @@ int main(){
 
 		move(0, 0);
 		refresh();
-		
+
 	}
 
 	return 0;
